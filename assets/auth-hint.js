4230cfccecde83103dd473d81c9ba6d352d1a802
Luci0n <lucianbascur@gmail.com>
2026-09-12 15:20:04 -0300

    Refresh focused tab transitions

--- assets/auth-hint.js
+++ assets/auth-hint.js
@@ -5,8 +5,8 @@
   script.defer = true;
   script.dataset.crowTopography = 'true';
   script.src = document.currentScript && document.currentScript.src
-    ? new URL('topography.js?v=topography-20260912k', document.currentScript.src).href
-    : 'assets/topography.js?v=topography-20260912k';
+    ? new URL('topography.js?v=topography-20260912l', document.currentScript.src).href
+    : 'assets/topography.js?v=topography-20260912l';
   document.head.appendChild(script);
 }());
 
@@ -17,7 +17,7 @@
   var style = document.createElement('link');
   style.rel = 'stylesheet';
   style.dataset.crowTopographyStyle = 'true';
-  style.href = source ? new URL('site.css?v=topography-20260912k', source).href : 'assets/site.css?v=topography-20260912k';
+  style.href = source ? new URL('site.css?v=topography-20260912l', source).href : 'assets/site.css?v=topography-20260912l';
   document.head.appendChild(style);
 }());
 
