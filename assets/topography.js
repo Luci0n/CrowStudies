deadc1da08e7e37790e6d02aea5150fef39826ad
Luci0n <lucianbascur@gmail.com>
2026-09-12 15:02:04 -0300

    Tune terrain grain to a fine visible texture

--- assets/topography.js
+++ assets/topography.js
@@ -160,7 +160,7 @@
       context.shadowBlur = 0;
     }
     if (texturePattern) {
-      context.globalAlpha = .88;
+      context.globalAlpha = .24;
       context.fillStyle = texturePattern;
       context.fillRect(0, 0, width, height);
       context.globalAlpha = 1;
