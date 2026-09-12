8ce5193801daed56a5189c037e0fc2d95b459de5
Luci0n <lucianbascur@gmail.com>
2026-09-12 14:35:45 -0300

    Refresh topographic assets after detail update

--- assets/auth-hint.js
+++ assets/auth-hint.js
@@ -5,8 +5,8 @@
   script.defer = true;
   script.dataset.crowTopography = 'true';
   script.src = document.currentScript && document.currentScript.src
-    ? new URL('topography.js?v=topography-20260912b', document.currentScript.src).href
-    : 'assets/topography.js?v=topography-20260912b';
+    ? new URL('topography.js?v=topography-20260912c', document.currentScript.src).href
+    : 'assets/topography.js?v=topography-20260912c';
   document.head.appendChild(script);
 }());
 
@@ -17,7 +17,7 @@
   var style = document.createElement('link');
   style.rel = 'stylesheet';
   style.dataset.crowTopographyStyle = 'true';
-  style.href = source ? new URL('site.css?v=topography-20260912', source).href : 'assets/site.css?v=topography-20260912';
+  style.href = source ? new URL('site.css?v=topography-20260912c', source).href : 'assets/site.css?v=topography-20260912c';
   document.head.appendChild(style);
 }());
 
