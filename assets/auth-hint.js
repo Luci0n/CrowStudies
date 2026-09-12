f64ee0fa44651de5de53ae6dff065e31ab1b975d
Luci0n <lucianbascur@gmail.com>
2026-09-12 14:57:09 -0300

    Refresh integrated terrain surface

--- assets/auth-hint.js
+++ assets/auth-hint.js
@@ -5,8 +5,8 @@
   script.defer = true;
   script.dataset.crowTopography = 'true';
   script.src = document.currentScript && document.currentScript.src
-    ? new URL('topography.js?v=topography-20260912f', document.currentScript.src).href
-    : 'assets/topography.js?v=topography-20260912f';
+    ? new URL('topography.js?v=topography-20260912g', document.currentScript.src).href
+    : 'assets/topography.js?v=topography-20260912g';
   document.head.appendChild(script);
 }());
 
@@ -17,7 +17,7 @@
   var style = document.createElement('link');
   style.rel = 'stylesheet';
   style.dataset.crowTopographyStyle = 'true';
-  style.href = source ? new URL('site.css?v=topography-20260912f', source).href : 'assets/site.css?v=topography-20260912f';
+  style.href = source ? new URL('site.css?v=topography-20260912g', source).href : 'assets/site.css?v=topography-20260912g';
   document.head.appendChild(style);
 }());
 
